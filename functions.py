b5be5d680363fb92d1875bc70eb644b47a7aef45
Seu Nome <seu@email.com>
2025-05-26 11:54:18 -0300

    update: webdriver is ligther and faster

--- functions.py
+++ functions.py
@@ -59,19 +59,21 @@ def k_filter(keywords,p_name):
 
 
 
-# === HEADLESS DRIVER FUNCTION ==== 
+# === HEADLESS AND LIGHT DRIVER FUNCTION ==== 
 
-
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def get_headless_driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")  
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920x1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--window-size=1920x1080")
 
-    driver = webdriver.Chrome(options=options)
-    return driver
+    return webdriver.Chrome(options=options)
